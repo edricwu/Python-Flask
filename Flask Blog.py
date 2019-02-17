65a131332f3d60f31c9a63b3ab9467cc6bc4c6e0
Edric <edricwu008@gmail.com>
2019-02-17 22:25:33 +0800

    added htmls

--- Flask Blog.py
+++ Flask Blog.py
@@ -1,9 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from config import Config
 
-@app.route("/")
-def hello():
-    return "<h1>Hello World1!</h1>"
+app = Flask(__name__)
+app.config.from_object(Config)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
 
 
 if __name__ == "__main__":
