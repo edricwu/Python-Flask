382c9ec7292340db1fefb90c9e253ca7e0312e25
Edric <edricwu008@gmail.com>
2019-02-17 23:08:02 +0800

    login form

--- Flask Blog.py
+++ Flask Blog.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from config import Config
+from forms import LoginForm
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +20,14 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash("Login successful for user {}, remember_me = {}" .format(form.username.data,form.remember_me.data))
+		return redirect({{ url_for('login') }})
+	return render_template('login.html', title = "Sign In", form = form)
 
 
 
